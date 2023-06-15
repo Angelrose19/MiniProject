45aac18e12d082c790f6207b0eac729dbbcf1fc7
Angelrose19 <angelroseinchodikkaran@gmail.com>
2023-06-15 14:12:28 +0530

    Co-authored-by: Bijo Mathew Jose <bijomathewjose@gmail.com>

--- FrontEnd/src/services/gateway.js
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-const baseURL = "https://asia-south1-miniproject-d3fad.cloudfunctions.net";
-
-export const publicGateway = axios.create({
-    baseURL,
-    headers: { "Content-Type": "application/json" },
-});
